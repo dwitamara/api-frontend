14e2a44189bc2b9fe76a59f7a047003feb5a439b
CarittaE <carittaelizabeth03@gmail.com>
2025-05-26 19:47:01 +0700

    edit

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,17 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import SewaLapangan from '../views/SewaLapangan.vue';
 import Community from '../views/Community.vue';
-import News from '../views/News.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import DetailLapangan from '../views/DetailLapangan.vue';
+import DetailPembayaran from '../views/DetailPembayaran.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/sewa-lapangan', component: SewaLapangan },
   { path: '/community', component: Community },
-  { path: '/news', component: News },
   { path: '/login', component: Login },
-  { path: '/register', component: Register }
+  { path: '/register', component: Register },
+  { path: '/detail-lapangan', component: DetailLapangan },
+  { path: '/detail-pembayaran', component: DetailPembayaran }
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/DetailLapangan.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Selamat datang di Halaman Home Pemesanan Lapangan!</h1>
+    <p>Pilih lapangan yang ingin kamu pesan!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailLapangan',
+}
+</script>
